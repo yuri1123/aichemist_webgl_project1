816c5d4e57271afd9413b4cb23f6d79d1966da44
choi yuri <yurifrom1123@gmail.com>
2023-04-07 14:42:13 +0900

    commit

---
+++ src/main/java/kr/aichemist/aichemist_webgl_project/FirebaseInitializer.java
@@ -0,0 +1,19 @@
+package kr.aichemist.aichemist_webgl_project;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+public class FirebaseInitializer {
+    public static void initialize() throws IOException {
+        FileInputStream serviceAccount =
+                new FileInputStream("path/to/serviceAccountKey.json");
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+        FirebaseApp.initializeApp(options);
+    }
+}
