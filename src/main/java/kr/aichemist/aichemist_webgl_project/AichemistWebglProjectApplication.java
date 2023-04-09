4b423431b11eed30c244b018bbf3200da50801ae
Choi Yuri <yurifrom1123@gmail.com>
2023-04-09 22:21:38 +0900

    commit

--- src/main/java/kr/aichemist/aichemist_webgl_project/AichemistWebglProjectApplication.java
+++ src/main/java/kr/aichemist/aichemist_webgl_project/AichemistWebglProjectApplication.java
@@ -2,9 +2,16 @@ package kr.aichemist.aichemist_webgl_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AichemistWebglProjectApplication {
+public class AichemistWebglProjectApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+        return builder.sources(AichemistWebglProjectApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AichemistWebglProjectApplication.class, args);
